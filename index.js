fbf355405d0f1fd65acb34b41a8b64e503a74df4
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2015-08-14 13:52:58 +0300

    Experimenting with tape-run and custom tools

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var browserRun = require('browser-run')
 var finished = require('tap-finished');
 var through = require('through');
 var throughout = require('throughout');
+var yorick = require('yorick');
 
 module.exports = run;
 
@@ -10,11 +11,13 @@ function run (opts) {
 
   var input = through();
   var browser = browserRun(opts);
+  var server = yorick({root: './test/fixtures', prefix: "/api/v1"});
   var dpl = throughout(input, browser);
 
   browser
     .pipe(finished(opts, function (results) {
       browser.stop();
+      server.stop();
       dpl.emit('results', results);
     }));
 
